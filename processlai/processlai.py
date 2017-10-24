d6c9d5ccd2227307b6f453e35bd54c54e4beaa6a
bucricket <bucricket@gmail.com>
2017-10-24 19:05:00 -0400

    Added a print statement

--- processlai/processlai.py
+++ processlai/processlai.py
@@ -461,6 +461,7 @@ def get_LAI(loc,start_date,end_date,usgs_user,usgs_pass,earth_user,
     search_df = getlandsatdata.search(loc[0],loc[1],start_date,end_date,cloud,available,cacheDir,sat)
     productIDs = search_df.LANDSAT_PRODUCT_ID
     paths = search_df.local_file
+    print(paths[0])
     # download MODIS LAI over the same area and time
     print("Downloading MODIS data...")
     get_modis_lai(tiles,MODIS_product,version,start_date,end_date,("%s"% earth_user,"%s"% earth_pass))
