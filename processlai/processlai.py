7f2f9c2b5f4cf5f66a127acc118ac392df1a4c8d
bucricket <bucricket@gmail.com>
2019-02-28 12:16:02 -0500

    since I am using conda-build 3 I no longer need to specify the compiler so I removed it...hope this works

--- processlai/processlai.py
+++ processlai/processlai.py
@@ -21,7 +21,7 @@ import keyring
 import sqlite3
 from pyproj import Proj
 from .utils import folders
-import wget
+import wgret
 # from getlandsatdata import getlandsatdata
 import pycurl
 from .landsatTools import landsat_metadata
